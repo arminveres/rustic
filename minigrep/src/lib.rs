bfe1faef2b85486d89279ca7f17e18a34b4bc7ca
Armin Veres <armin.veres@hotmail.com>
2023-03-25 21:31:28 +0100

    Added pathbuf path parsing + ignore case opt

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,11 +1,12 @@
-use std::{env, error::Error, fs, process};
+use std::{env, error::Error, fs};
 
 // I chose to try it with lifetimes, the book uses Strings. It argues, that while cloning the
 // string slice references would be later, using the Strings is easier to handle... let's see !
 #[derive(Debug)]
 pub struct Config<'a> {
     query: &'a str,
-    file_path: &'a str,
+    file_path: std::path::PathBuf,
+    ignore_case: bool,
 }
 
 impl<'a> Config<'a> {
@@ -13,17 +14,44 @@ impl<'a> Config<'a> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
+
         let query = &args[1];
         let file_path = &args[2];
-        Ok(Self { query, file_path })
+
+        // NOTE: (aver) another possibility: https://rust-cli.github.io/book/tutorial/cli-args.html
+        // let query = env::args().nth(1).expect("No query given");
+        // let file_path = env::args().nth(2).expect("No path given");
+
+        let mut ignore_case = env::var("MINIGREP_IGNORE_CASE").is_ok();
+
+        match args.get(3) {
+            Some(arg) => match arg.as_str() {
+                "-i" | "--ignore-case" => ignore_case = true,
+                _ => {}
+            },
+            None => {}
+        }
+
+        Ok(Self {
+            query,
+            file_path: std::path::PathBuf::from(file_path),
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    for line in search(&config.query, &contents) {
+
+    let results = if config.ignore_case {
+        case_insensitive_search(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
         println!("{line}");
     }
+
     Ok(())
 }
 
@@ -40,17 +68,47 @@ pub fn search<'a>(
     results
 }
 
+pub fn case_insensitive_search<'a>(
+    query: &str,
+    contents: &'a str, // contents will be needing the string slices for its lifetime, not query
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "dUcT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            case_insensitive_search(query, contents)
+        );
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,31 @@
 use minigrep::{run, Config};
 use std::{env, process};
 
+// use clap::Parser;
+// #[derive(clap::Parser)]
+// pub struct Config {
+//     /// Query to look for
+//     query: String, //&'a str,
+//     /// Path to look in for
+//     file_path: std::path::PathBuf, //&'a str,
+//     #[arg(long,short)]
+//     ignore_case: Option<bool>
+// }
+
 fn main() {
+    // let args = Config::parse();
+
     // Use args instead of args_os (which would accept utf-8 strings), because it returs Strings,
     // which are easier to work with
     let args = env::args().collect::<Vec<String>>(); // collect is one of those functions that need explicit type
                                                      // annotations
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!(
+            "Problem parsing arguments: {err}.
+Usage: [query] [path] ..
+    --ignore-case, -i: case insensitive search"
+        );
         process::exit(1);
     });
 
